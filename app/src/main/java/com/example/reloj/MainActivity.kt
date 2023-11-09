6a0fae38082d2f159b395ad82dadfa1866bcafc6
samuelws <samuelsolanoama@gmail.com>
2023-11-09 10:35:00 -0500

    Tamo lito Profe

--- app/src/main/java/com/example/reloj/MainActivity.kt
+++ app/src/main/java/com/example/reloj/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.reloj
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
 import com.example.reloj.databinding.ActivityMainBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +24,31 @@ class MainActivity : AppCompatActivity() {
         binding.button2.setOnClickListener {
             startActivity(Intent(this, Temporizador::class.java))
         }
+        var k = object : CountDownTimer(9999999999, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                a()
+            }
+
+            override fun onFinish() {
+            }
+        }.start()
+
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        a()
+    }
+
+    fun Hora() {
+        val time = Calendar.HOUR_OF_DAY
+        binding.aaaa.text = time.toString()
+    }
+
+
+    fun a() {
+        val fecha3 = DateTimeFormatter.ofPattern("HH:mm:ss")
+        binding.aaaa.text = (fecha3.format(LocalDateTime.now()))
     }
 }
